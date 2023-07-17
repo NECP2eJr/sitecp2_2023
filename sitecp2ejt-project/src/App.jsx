6afa4b9f2d980b2980bcf50edb4cda45cf7cd63b
Matheus Julidori <matheusjulidori@cp2ejr.com.br>
2023-07-17 19:59:09 +0200

    Servicos

--- sitecp2ejt-project/src/App.jsx
+++ sitecp2ejt-project/src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import Servicos from './components/Servicos'
 import Home from './components/Home'
 import Header from './components/Header'
 import Servicos from './components/Servicos'
