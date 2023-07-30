b18acf854bfa666f07482becee558536e3f5c973
Alice Coelho <alice.coelho@ges.inatel.br>
2023-07-30 19:15:57 -0300

    (RCP2-17) Página PS

--- sitecp2ejt-project/src/App.jsx
+++ sitecp2ejt-project/src/App.jsx
@@ -3,6 +3,7 @@ import Home from './components/Home'
 import Header from './components/Header'
 import Servicos from './components/Servicos'
 import MVV from './components/MVV'
+import PS from './components/PS'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Home/>
       <Servicos />
       <MVV/>
+      <PS/>
     </>
   )
 }
